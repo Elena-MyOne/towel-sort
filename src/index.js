0577342be556761cc02c8c5383adaea597cc9e2e
Elena-MyOne <yakovenko.ea72@gmail.com>
2023-08-07 15:35:49 -0400

    feat: add solution for towelSort, previous deleted repo

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix) return [];
+    const arr2 = matrix.map((item, index) => {
+        if (index % 2 !== 0) {
+            return item.reverse();
+        }
+        return item;
+    });
+    return arr2.flat();
 }
